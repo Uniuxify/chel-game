3962a091b98500f251cf91b2b0baec84b1bdda13
Uniuxify <Uniuxify@gmail.com>
2024-01-11 18:06:27 +0300

    Added conv_types module

---
+++ src/core/conv_types.py
@@ -0,0 +1,14 @@
+from typing import Union
+import src.core.geometry as geom
+import pygame
+
+tPoint = Union[tuple[float, float], geom.Point]
+tSurface = Union[pygame.Surface, pygame.SurfaceType]
+
+
+def to_point(point: tPoint) -> geom.Point:
+    if isinstance(point, tuple):
+        return geom.Point.from_tuple(point)
+    if isinstance(point, geom.Point):
+        return point
+    raise TypeError()
