c522dfaa8b3935746fe899da7d982aba5c6a5e1f
Uniuxify <Uniuxify@gmail.com>
2024-01-11 18:08:58 +0300

    Added animation module

---
+++ src/core/animation.py
@@ -0,0 +1,144 @@
+from typing import List, Literal
+from src.core.conv_types import tSurface
+
+from src.core.geometry import Point
+import enum
+
+from collections import defaultdict
+
+
+class Frame:
+    def __init__(self, image: tSurface, pivots: List[Point] = None):
+        self.image = image
+        self.pivots = pivots
+
+    def render(self, screen, pos):
+        screen.blit(self.image, pos)
+
+
+class FramesOrder(enum.Enum):
+    reverse = 'reverse'
+    default = 'default'
+    back_and_forth = 'back-and-forth'
+    forth_and_back = 'forth-and-back'
+
+    @staticmethod
+    def values():
+        return tuple([e.value for e in FramesOrder])
+
+
+class EventManager:
+    def __init__(self):
+        self.handlers = defaultdict(list)
+
+    def subscribe(self, event_type, handler):
+        self.handlers[event_type].append(handler)
+
+    def unsubscribe(self, event_type, handler):
+        self.handlers[event_type].remove(handler)
+
+    def notify(self, event_type, *args, **kwargs):
+        for handler in self.handlers[event_type]:
+            handler(*args, **kwargs)
+
+
+class AnimationEvents(enum.Enum):
+    animation_ended = 0
+    new_loop = 1
+
+
+class Animation:
+    def __init__(self, frames: tuple[Frame, ...],  # TODO: Добавить проверку того, что все кадры одинакового размер
+                 on_animation_end=None,
+                 frames_order: Literal['default', 'reverse', 'forth-and-back', 'back-and-forth'] = 'default',
+                 # frames_order=FramesOrder.default,
+                 loop: int = -1):
+
+        if loop == 0:
+            raise ValueError('loop cannot be zero')
+        # if frames_order not in FramesOrder.values():
+        #     raise ValueError(f'frames_order must be one of {FramesOrder.values()}, not {frames_order}')
+
+        self.frames = frames
+
+        self.frames_order = frames_order
+
+        if self.frames_order in (FramesOrder.reverse.value, FramesOrder.back_and_forth.value):
+            self.curr_frame = self.last_frame()
+        else:
+            self.curr_frame = 0
+
+        self.loops_left = loop
+        self.loop = loop
+        self.paused = False
+
+        self.step = 1 if frames_order in (FramesOrder.default.value, FramesOrder.forth_and_back.value) else -1
+
+        self.on_animation_end = on_animation_end
+
+        self.event_manager = EventManager()
+        self.is_ended = False
+
+    def __iter__(self):
+        self.loops_left = self.loop
+        if self.frames_order == FramesOrder.default.value:
+            return iter(self.frames)
+        elif self.frames_order == FramesOrder.reverse.value:
+            return iter(reversed(self.frames))
+        elif self.frames_order == FramesOrder.back_and_forth.value:
+            return iter(tuple(reversed(self.frames)) + self.frames)
+        elif self.frames_order == FramesOrder.forth_and_back.value:
+            return iter(self.frames + tuple(reversed(self.frames)))
+
+    def __next__(self) -> Frame:
+        frame = self.frames[self.curr_frame]
+        print(self.curr_frame)
+        if self.paused or self.is_ended:
+            return frame
+
+        self.curr_frame += self.step
+        if self.curr_frame == self.last_frame() + 1:
+            if self.frames_order == FramesOrder.default.value or self.frames_order == FramesOrder.back_and_forth.value:
+                self.curr_frame = 0
+                self.loops_left -= 1
+                self.event_manager.notify(AnimationEvents.new_loop)
+            if self.frames_order == FramesOrder.forth_and_back.value or self.frames_order == FramesOrder.back_and_forth.value:
+                self.step *= -1
+                self.curr_frame += self.step
+
+        elif self.curr_frame == -1:
+            if self.frames_order == FramesOrder.reverse.value or self.frames_order == FramesOrder.forth_and_back.value:
+                self.loops_left -= 1
+                self.event_manager.notify(AnimationEvents.new_loop)
+                self.curr_frame = self.last_frame()
+            if self.frames_order == FramesOrder.back_and_forth.value or self.frames_order == FramesOrder.forth_and_back.value:
+                self.step *= -1
+                self.curr_frame += self.step
+
+        if self.loops_left == 0:
+            self.loops_left = self.loop
+            self.is_ended = True
+
+            self.event_manager.notify(AnimationEvents.animation_ended)
+        return frame
+
+    def n_frames(self):
+        return len(self.frames)
+
+    def pause(self):
+        self.paused = True
+
+    def unpause(self):
+        self.paused = False
+
+    def reset(self):
+        self.unpause()
+        self.is_ended = False
+        self.loops_left = self.loop
+        if self.frames_order in (FramesOrder.reverse.value, FramesOrder.back_and_forth.value):
+            self.curr_frame = self.last_frame()
+        else:
+            self.curr_frame = 0
+
+    def last_frame(self):
+        return len(self.frames) - 1
